a29a840009003f69ff3c38f92d192d75b9c9435e
Bivek Thapa <beingbivek@gmail.com>
2024-09-09 08:16:30 +0545

    D1 C Programming Structures

---
+++ Week1/day1.c
@@ -0,0 +1,38 @@
+//1. Documentation Section (Optional)
+/*
+Author: Bivek Thapa
+Date: 09/04/2024
+Purpose: A simple program to print Hello World
+version: 1.0.0
+*/
+
+//2. Linking Section (Mandatory)
+#include<stdio.h> //stdio = standard input and output
+
+//3. Defination section (Optional)
+#define PI 3.14
+
+//4. Global Declaration Section (Optional)
+int pi = 3.14;
+
+//5. Main Section (Mandatory)
+int main(){
+    // single line comment
+
+    /*
+    This is multiline 
+    comments
+    */
+
+    // printf("Hello World");
+    printf("Hello World");
+
+    return 0; // return 0 is code executed without errors and return 1 is code executed with some errors (Optional)
+}
+
+//6. Sub Functional Section (Optional)
+int sum(){}
+
+int calculate(){}
+
+int divide(){}

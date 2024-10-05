4ceec20c707811c8af1fb5577686fda41677ec5d
Bivek Thapa <beingbivek@gmail.com>
2024-10-05 10:01:45 +0545

    tiktactoe in solving

--- Week3/Array/tiktactoe.c
+++ Week3/Array/tiktactoe.c
@@ -3,11 +3,11 @@
 #include<stdio.h>
 
 int main(){
-    int row=3,col=3,mainchoice,num,gameover = 0;
+    int row,col,mainchoice,num,gameover = 0;
     char playerchoice,player2choice;
-    char ttt[row][col];
+    char ttt[3][3]={{"1","2","3"},{"4","5","6"},{"7","8","9"}},usedchar[3][3]={{"a","a","a"},{"a","a","a"},{"a","a","a"}};
     printf("Welcome to 3 * 3 TicTacToe Game - developed by Bivek\n");
-    printf("Main Menu\n1. with PC\n2. with Human\n3. Exit\n: ");
+    printf("Main Menu\n1. with PC (Not available)\n2. with Player 2\n3. Exit\n: ");
     scanf("%d",&mainchoice);
     switch (mainchoice)
     {
@@ -16,23 +16,63 @@ int main(){
         break;
     case 2:
         printf("You are Player 1\nYour friend is Player 2\n");
+        while(playerchoice == "x" || playerchoice == "o")
+        {
+            printf("\nPlayer 1 choose x or o (only small letters): ");
+            scanf("%c",&playerchoice);
+        }
+        if(playerchoice == "x"){
+            player2choice = "o";
+        }else{
+            player2choice = "x";
+        }
         printf("1|2|3\n------\n4|5|6\n------\n7|8|9\n");
-        printf("Player 1 choose x or o (only small letters): ");
-        scanf("%c",&playerchoice);
-        printf("Where do you want to start?\n");
+        printf("\nWhere do you want to start?\n");
         while (gameover == 1)
         {
-        printf("Enter number: ");
-        scanf("%d",&num);
-        switch (num)
-        {
-        case 1:
-            ttt[0][0] = playerchoice;
-            break;
-        
-        default:
-            break;
-        }
+            printf("Enter number: ");
+            scanf("%d",&num);
+            for(int i = 0; i < 3; i++){
+                for(int j = 0; j <3; j++){
+                    if(ttt[i][j] != num + '0'){
+                        usedchar[i][j] = 
+                    }
+                }
+            }
+            switch (num)
+            {
+            case 1:
+                ttt[0][0] = playerchoice;
+                break;
+            case 2:
+                ttt[0][1] = playerchoice;
+                break;
+            case 3:
+                ttt[0][2] = playerchoice;
+                break;
+            case 4:
+                ttt[1][0] = playerchoice;
+                break;
+            case 5:
+                ttt[1][1] = playerchoice;
+                break;
+            case 6:
+                ttt[1][2] = playerchoice;
+                break;
+            case 7:
+                ttt[2][0] = playerchoice;
+                break;
+            case 8:
+                ttt[2][1] = playerchoice;
+                break;
+            case 9:
+                ttt[2][2] = playerchoice;
+                break;
+            
+            default:
+                printf("Invalid number, Try again (1-9)!\n");
+                break;
+            }
         }
 
 
@@ -42,6 +82,7 @@ int main(){
         break;
     
     default:
+        printf("Invalid Selection, Try again!\n");
         break;
     }
     printf("1|2|3\n------\n4|5|6\n------\n7|8|9\n");
